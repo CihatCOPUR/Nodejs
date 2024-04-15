24db3d53fca92447fa94671fb50aad2cd4206a10
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-04-15 20:20:11 +0300

    user model

--- src/models/user.js
+++ src/models/user.js
@@ -36,3 +36,39 @@ const { mongoose } = require('../configs/dbConnection')
     "isAdmin": false
 }
 /* ------------------------------------------------------- */
+
+const userSchema= new mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        index:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        unique:true,
+        required:true,
+        index:true
+
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+},{
+    collection:'users',
+    timestamps:true
+})
